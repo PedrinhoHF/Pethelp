83d6e1222c7da90bdfc3963d3f3f8c1b4cd8fe63
Spinelli13 <matheus.cspinelli@gmail.com>
2022-03-17 03:48:24 -0300

    Adicionando abas e CSS

--- routes/rotasClientes.js
+++ routes/rotasClientes.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const express = require('express');
 const router = express.Router();
 
@@ -8,15 +8,5 @@ router.get('/',ClientesController.index);
 router.get('/cadastroPets',ClientesController.cadastroPets);
 
 
-=======
-const express = require('express');
-const router = express.Router();
 
-const ClientesController = require('../controllers/ClientesController');
-
-router.get('/',ClientesController.index);
-router.get('/cadastroPets',ClientesController.cadastroPets);
-
-
->>>>>>> 33016e8ee8766cffd362a7381a16ea2c08f9046f
 module.exports = router;
