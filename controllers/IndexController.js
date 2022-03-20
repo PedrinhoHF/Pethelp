d2b68ff327da144cdefa54c63fc64c28364462f1
WillameFarley <willamefarley@gmail.com>
2022-03-20 18:17:18 -0300

    Atualizações cadVet

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-const IndexController = {
-
+const indexController = {
     index: (req,res) =>{
         res.render('index');
     },
     indexPets:(req,res) =>{
-
         res.send("Pet Help!")
     }
-    
 }
 
-module.exports = IndexController;
-=======
-
->>>>>>> 714611098936f89b994187278ee5fb17e20a1ae3
+module.exports = indexController;
