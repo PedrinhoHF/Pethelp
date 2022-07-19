f26bb56856b2724060d63888d72da30708c2d863
Matheus Spinelli <sam@google.comsam@google.com>
2022-07-19 13:32:17 -0300

    Atualização Modal

--- public/javascripts/modalCliente.js
+++ public/javascripts/modalCliente.js
@@ -1,3 +1,5 @@
+// Modal Adicionar PET
+
 const openModal = () =>
   document.getElementById("modal").classList.add("active");
 
@@ -9,6 +11,43 @@ document
   .querySelectorAll(".AdicionarCadPet")
   .forEach((element) => element.addEventListener("click", openModal));
 document.getElementById("modalClose").addEventListener("click", closeModal);
+
+
+
+// Modal Remarcar Consulta
+
+const openModalAgenda = () =>
+  document.getElementById("modal-agenda").classList.add("active");
+
+const closeModalAgenda = () => {
+  document.getElementById("modal-agenda").classList.remove("active");
+};
+
+document
+  .querySelectorAll("#Remarcar")
+  .forEach((element) => element.addEventListener("click", openModalAgenda));
+document.getElementById("modalCloseAgenda").addEventListener("click", closeModalAgenda);
+
+// Modal Nova Consulta
+
+const openModalNovaConsulta = () =>
+  document.getElementById("modalNovaConsulta").classList.add("active");
+
+const closeModalNovaConsulta = () => {
+  document.getElementById("modalNovaConsulta").classList.remove("active");
+};
+
+document
+  .querySelectorAll(".AgendarConsulta-btn")
+  .forEach((element) => element.addEventListener("click", openModalNovaConsulta));
+document.getElementById("modalCloseNovaConsulta").addEventListener("click", closeModalNovaConsulta);
+
+
+
+
+
+
+// Modal Relatorio 
 
 const openModalRelatorio = () =>
   document.getElementById("modalRelatorio").classList.add("active");
